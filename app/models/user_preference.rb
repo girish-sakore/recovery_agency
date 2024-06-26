8e0cd0736b22b1f66fcb8ee905a51cd935d1968c
g-WSL <gsakore22@gmail.com>
2024-06-26 23:21:22 +0530

    column_preferences failsafe

--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -1,4 +1,13 @@
 class UserPreference < ApplicationRecord
   belongs_to :admin_user
   # serialize :column_preferences, JSON
+  def column_preferences
+    super || default_columns
+  end
+
+  private
+
+  def default_columns
+    ['id', 'customer_name']  # Add your default columns here
+  end
 end
